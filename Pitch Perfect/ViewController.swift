caff9b39d29abfdce3c21b7f598f469daac7b2c5
Sri Chandra Mallipeddi <smallipeddi@google.com>
2016-02-17 21:31:01 -0800

    Version 1 of App

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        recordingInProgress.hidden = false
+        //TODO: Record the user's voice
+        print("Recording");
+    }
 
+    @IBAction func stopAudio(sender: AnyObject) {
+        recordingInProgress.hidden = true
+    }
 }
 
