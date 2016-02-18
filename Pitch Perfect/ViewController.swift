e98517cb1fb31e51ce92ea7a08de35ae5b82ca26
Sri Chandra Mallipeddi <smallipeddi@google.com>
2016-02-17 23:23:15 -0800

    UI changes and playing audio

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,15 +24,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+        recordButton.enabled = true
+
+    }
 
     @IBAction func recordAudio(sender: AnyObject) {
+        stopButton.hidden = false
         recordingInProgress.hidden = false
+        recordButton.enabled = false
         //TODO: Record the user's voice
         print("Recording");
     }
 
     @IBAction func stopAudio(sender: AnyObject) {
         recordingInProgress.hidden = true
+        //recordButton.enabled = true
     }
 }
 
---
+++ Pitch Perfect/playSoundsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  playSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Sri Chandra Mallipeddi on 2/17/16.
+//  Copyright © 2016 Sri Chandra Mallipeddi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class playSoundsViewController: UIViewController {
+
+    var audioPlayer:AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            let filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = try!
+            AVAudioPlayer(contentsOfURL: filePathUrl)
+            audioPlayer.enableRate = true
+        }else {
+            print("The filepath is empty")
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func playFastAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 1.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopPlayingAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
